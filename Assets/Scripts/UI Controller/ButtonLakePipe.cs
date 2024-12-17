37797cdde350e77b7132eb31c374e157510359cc
Sulthon-Baiquni <102643599+Sulthon-Baiquni@users.noreply.github.com>
2024-12-17 13:51:08 +0700

    update

--- Assets/Scripts/UI Controller/ButtonLakePipe.cs
+++ Assets/Scripts/UI Controller/ButtonLakePipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RandomLake : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     [System.Serializable]
     public class PipeInfo
